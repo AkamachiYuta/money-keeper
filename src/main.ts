0d36d0741f96ec528e4b407b32319a6cf63f203f
AkamachiYuta <yuta@akamachi.jp>
2025-04-17 04:17:42 +0900

    Add: 基本のページ、画面実装

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import "beercss";
 import "material-dynamic-colors";
-import './style.css'
+import "./style.scss"
+
+document.querySelectorAll("#nav-drawer a[data-ui]").forEach(a => {
+  a.addEventListener("click", () => {
+    ui('#nav-drawer');
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig, loadEnv } from 'vite';
+import { viteSingleFile } from "vite-plugin-singlefile";
+import packageJson from './package.json'
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   const define = {
     __APP_TITLE__: process.env.VITE_APP_TITLE ? process.env.VITE_APP_TITLE : "Unknown App Title",
-    __APP_VERSION__: process.env.npm_package_version ? process.env.npm_package_version + (mode === "development" ? "-DEV" : "") : "Unknown Version",
+    __APP_SHORT_TITLE__: process.env.VITE_APP_SHORT_TITLE ? process.env.VITE_APP_SHORT_TITLE : "Unknown App Short Title",
+    __APP_VERSION__: packageJson.version ? packageJson.version + (mode === "development" ? "-DEV" : "") : "Unknown Version",
+    __APP_PROVIDER__: process.env.VITE_APP_PROVIDER ? process.env.VITE_APP_PROVIDER : "Unknown App Provider",
+    __APP_PROVIDER_URL__: process.env.VITE_APP_PROVIDER_URL ? process.env.VITE_APP_PROVIDER_URL : "",
+    __APP_DESCRIPTION__: packageJson.description ? packageJson.description : "",
+    __APP_REPOSITORY_URL__: packageJson.repository.url ? packageJson.repository.url : "",
   }
   return {
     root: 'src',
@@ -16,20 +23,19 @@ export default defineConfig(({ mode }) => {
       port: 4810,
     },
     plugins: [
+      viteSingleFile(),
       {
         name: "html-transform",
         transformIndexHtml: {
-          enforce: "pre" as const,
-          transform: (html: string): string =>
-            html.replace(/%(.*?)%/g, (match, p1) => {
-              return define[p1] ?? match
-            }),
+          order: "pre" as const,
+          handler: (html: string): string =>
+            html.replace(/%(.*?)%/g, (match, p1) =>
+              define[p1] ?? match),
         },
       },
     ],
     define: Object.fromEntries(
-      Object.entries(define)
-        .map(([key, val]) => [key, JSON.stringify(val)])
+      Object.entries(define).map(([key, val]) => [key, JSON.stringify(val)])
     ),
   }
 });
