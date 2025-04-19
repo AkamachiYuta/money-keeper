5c7931c467203637ac4af41f633116e792fb5e3f
AkamachiYuta <yuta@akamachi.jp>
2025-04-20 01:07:55 +0900

    Develop: Svelte導入

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
+/// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare const APP_TITLE: string;
+declare const APP_SHORT_TITLE: string;
+declare const APP_VERSION: string;
+declare const APP_PROVIDER: string;
+declare const APP_PROVIDER_URL: string;
+declare const APP_DESCRIPTION: string;
+declare const APP_REPOSITORY_URL: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig, loadEnv } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { viteSingleFile } from "vite-plugin-singlefile";
 import packageJson from "./package.json";
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   const define = {
-    __APP_TITLE__: process.env.VITE_APP_TITLE ? process.env.VITE_APP_TITLE : "Unknown App Title",
-    __APP_SHORT_TITLE__: process.env.VITE_APP_SHORT_TITLE ? process.env.VITE_APP_SHORT_TITLE : "Unknown App Short Title",
-    __APP_VERSION__: packageJson.version ? packageJson.version + (mode === "development" ? "-DEV" : "") : "Unknown Version",
-    __APP_PROVIDER__: process.env.VITE_APP_PROVIDER ? process.env.VITE_APP_PROVIDER : "Unknown App Provider",
-    __APP_PROVIDER_URL__: process.env.VITE_APP_PROVIDER_URL ? process.env.VITE_APP_PROVIDER_URL : "",
-    __APP_DESCRIPTION__: packageJson.description ? packageJson.description : "",
-    __APP_REPOSITORY_URL__: packageJson.repository.url ? packageJson.repository.url.replace(/^git\+|\.git$/g, "") : "",
+    APP_TITLE: process.env.VITE_APP_TITLE ? process.env.VITE_APP_TITLE : "Unknown App Title",
+    APP_SHORT_TITLE: process.env.VITE_APP_SHORT_TITLE ? process.env.VITE_APP_SHORT_TITLE : "Unknown App Short Title",
+    APP_VERSION: packageJson.version ? packageJson.version + (mode === "development" ? "-DEV" : "") : "Unknown Version",
+    APP_PROVIDER: process.env.VITE_APP_PROVIDER ? process.env.VITE_APP_PROVIDER : "Unknown App Provider",
+    APP_PROVIDER_URL: process.env.VITE_APP_PROVIDER_URL ? process.env.VITE_APP_PROVIDER_URL : "",
+    APP_DESCRIPTION: packageJson.description ? packageJson.description : "",
+    APP_REPOSITORY_URL: packageJson.repository.url ? packageJson.repository.url.replace(/^git\+|\.git$/g, "") : "",
   };
   return {
     root: "src",
@@ -23,16 +24,17 @@ export default defineConfig(({ mode }) => {
       port: 4810,
     },
     plugins: [
-      viteSingleFile(),
       {
         name: "html-transform",
         transformIndexHtml: {
           order: "pre" as const,
           handler: (html: string): string =>
-            html.replace(/%(.*?)%/g, (match, p1) =>
+            html.replace(/{(.*?)}/g, (match, p1) =>
               define[p1] ?? match),
         },
       },
+      svelte(),
+      viteSingleFile(),
     ],
     define: Object.fromEntries(
       Object.entries(define).map(([key, val]) => [key, JSON.stringify(val)])
